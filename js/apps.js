10798dd07b16a036c2cf0f802e0e8de58040c874
Bateyjosue <josuebatey19@gmail.com>
2022-07-19 12:35:08 +0200

    Fix linters error for html, css and js

--- js/apps.js
+++ js/apps.js
@@ -1,27 +1,27 @@
-let showMenu = document.querySelector('#show-menu');
-let closeMenu = document.querySelector('#close-menu');
-let menu = document.querySelector('ul.mobile-menu');
-let al = document.querySelectorAll('section')
-let menuList =  document.querySelectorAll('ul.mobile-menu li');
+const showMenu = document.querySelector('#show-menu');
+const closeMenu = document.querySelector('#close-menu');
+const menu = document.querySelector('ul.mobile-menu');
+const al = document.querySelectorAll('section');
+const menuList = document.querySelectorAll('ul.mobile-menu li');
 
-showMenu.addEventListener('click',()=>{
-    menu.style.visibility = 'visible';
-    al.forEach(el =>{
-        el.classList.add('blur');
-        el.style.backgroundColor='#505f79';
-    })
+showMenu.addEventListener('click', () => {
+  menu.style.visibility = 'visible';
+  al.forEach((el) => {
+    el.classList.add('blur');
+    el.style.backgroundColor = '#505f79';
+  });
 });
 
-closeMenu.addEventListener('click',()=>{
-    menu.style.visibility = 'hidden';
+closeMenu.addEventListener('click', () => {
+  menu.style.visibility = 'hidden';
 });
 
-menuList.forEach(el =>{
-    el.addEventListener('click',()=>{
+menuList.forEach((el) => {
+  el.addEventListener('click', () => {
     menu.style.visibility = 'hidden';
-    al.forEach(el =>{
-        el.classList.remove('blur');
-        el.style.backgroundColor='transparent';
-    })
-    })
+    al.forEach((el) => {
+      el.classList.remove('blur');
+      el.style.backgroundColor = 'transparent';
+    });
+  });
 });
